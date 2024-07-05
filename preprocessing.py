223bc848a8f8dddd9a3f7bfcbf8c50fa7eed003b
Maxim <maksimsazonov476@gmail.com>
2024-07-05 14:35:00 -0700

    changes py files

--- preprocessing.py
+++ preprocessing.py
@@ -3,7 +3,7 @@ from catboost.datasets import titanic
 # load
 titanic_train, titanic_test = titanic()
 
-titanic_train = titanic_train[['Polass', 'Sex', 'Age']]
+titanic_train = titanic_train[['Pclass', 'Sex', 'Age']]
 
 #Save
 titanic_train.to_csv('datasets/titanic_train.csv', index=False)
